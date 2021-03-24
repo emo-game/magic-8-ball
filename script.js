d573bd390ac47e889bb120d3c9b4019cf53c94c3
Léa <leareviron@yahoo.fr>
2021-03-24 12:46:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-function getAccel(){
-    DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-            console.log("accelerometer permission granted");
-            // Do stuff here
-        }
-    });
-}
 
 function getAccel(){
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
+            console.log("accelerometer permission granted");
         // Add a listener to get smartphone acceleration 
             // in the XYZ axes (units in m/s^2)
             window.addEventListener('devicemotion', (event) => {

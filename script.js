e89b4cdffac71ee72779064d1c8714f0dbf70216
Léa <leareviron@yahoo.fr>
2021-03-24 12:42:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function getAccel(){
     DeviceMotionEvent.requestPermission().then(response => {
         if (response == 'granted') {
-            console.log("accelerometer permission granted");
-            // Do stuff here
-        }
+        // Add a listener to get smartphone acceleration 
+            // in the XYZ axes (units in m/s^2)
+            window.addEventListener('devicemotion', (event) => {
+                console.log(event);
+            });       // Add a listener to get smartphone orientation 
+           // in the alpha-beta-gamma axes (units in degrees)
+            window.addEventListener('deviceorientation',(event) => {
+                console.log(event);
+            });        }
     });
 }

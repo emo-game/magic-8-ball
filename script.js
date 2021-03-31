6f6243dc817504f31483d7ce080cbab44b1d0f3d
Léa <leareviron@yahoo.fr>
2021-03-31 15:34:09 +0200

    change-script

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 
 let CURRENT_SEQ = null
 
-    function getAccel(){
-        DeviceMotionEvent.requestPermission().then(response => {
-        if (response == 'granted') {
-        // Add a listener to get smartphone acceleration 
-            // in the XYZ axes (units in m/s^2)
-            window.addEventListener('devicemotion', (event) => {
-                console.log(event);
-            });       // Add a listener to get smartphone orientation 
-           // in the alpha-beta-gamma axes (units in degrees)
-            window.addEventListener('deviceorientation',(event) => {
-                console.log(event);
-            });        }
-        });
-    }
+  
 
 var myShakeEvent = new Shake({
     threshold: 15, // optional shake strength threshold

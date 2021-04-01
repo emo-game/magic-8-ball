cf664d9f9c8a26ebbacaf94ecdca268f90676125
Léa <leareviron@yahoo.fr>
2021-04-01 11:12:01 +0200

    thfnyt + P5

--- script.js
+++ script.js
@@ -1,36 +1,74 @@
 
-let CURRENT_SEQ = null
+// let CURRENT_SEQ = null
 
-function Shake(options) {
-    //feature detect
-    this.hasDeviceMotion = 'ondevicemotion' in window;
+// function Shake(options) {
+//     //feature detect
+//     this.hasDeviceMotion = 'ondevicemotion' in window;
 
-    this.options = {
-        threshold: 15, //default velocity threshold for shake to register
-        timeout: 1000 //default interval between events
-    };
+//     this.options = {
+//         threshold: 15, //default velocity threshold for shake to register
+//         timeout: 1000 //default interval between events
+//     };
   
 
-}
-var myShakeEvent = new Shake({
-    threshold: 15, // optional shake strength threshold
-    timeout: 1000 // optional, determines the frequency of event generation
-});
+// }
+// var myShakeEvent = new Shake({
+//     threshold: 15, // optional shake strength threshold
+//     timeout: 1000 // optional, determines the frequency of event generation
+// });
 
-myShakeEvent.start();
+// myShakeEvent.start();
 
-window.addEventListener('shake', shakeEventDidOccur, false);
-
-
+// window.addEventListener('shake', shakeEventDidOccur, false);
 
 
 
-//function to call when shake occurs
 
-function shakeEventDidOccur () {
+
+// //function to call when shake occurs
+
+// function shakeEventDidOccur () {
    
   
 
-    console.log("shake ya booty man!");
+//     console.log("shake ya booty man!");
 
+// }
+
+var x, y, z;
+var px, py, pz; // p = precedent
+
+function setup(){
+    let div = createDiv("sequence img");
+    div.id('seq-img')
+    //createCanvas(windowWidth, windowHeight, WEBGL);
+  x = accelerationX;
+  y = accelerationY;
+  z = accelerationZ;
+  px = x;
+  py = y;
+  pz = z;
+  
+}
+
+function draw(){
+    shakeTrue();
+
+}
+
+function shakeTrue() {
+    x = accelerationX;
+    y = accelerationY;
+    z = accelerationZ;
+    
+    console.log(dist(x, y, z, px, py, pz));
+
+    if(dist(x, y, z, px, py, pz) > 20){
+        console.log("shake it shake it up");
+    }
+
+
+    px = x;
+    py = y;
+    pz = z;
 }
--- shake.js
+++ shake.js
@@ -1,129 +1,129 @@
-/*
- * Author: Alex Gibson
- * https://github.com/alexgibson/shake.js
- * License: MIT license
- */
+// /*
+//  * Author: Alex Gibson
+//  * https://github.com/alexgibson/shake.js
+//  * License: MIT license
+//  */
 
-(function(global, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(function() {
-            return factory(global, global.document);
-        });
-    } else if (typeof module !== 'undefined' && module.exports) {
-        module.exports = factory(global, global.document);
-    } else {
-        global.Shake = factory(global, global.document);
-    }
-} (typeof window !== 'undefined' ? window : this, function (window, document) {
-    
-    'use strict';
+// (function(global, factory) {
+//     if (typeof define === 'function' && define.amd) {
+//         define(function() {
+//             return factory(global, global.document);
+//         });
+//     } else if (typeof module !== 'undefined' && module.exports) {
+//         module.exports = factory(global, global.document);
+//     } else {
+//         global.Shake = factory(global, global.document);
+//     }
+// } (typeof window !== 'undefined' ? window : this, function (window, document) {
 
-    function Shake(options) {
-        //feature detect
-        this.hasDeviceMotion = 'ondevicemotion' in window;
+//     'use strict';
 
-        this.options = {
-            threshold: 15, //default velocity threshold for shake to register
-            timeout: 1000 //default interval between events
-        };
+//     function Shake(options) {
+//         //feature detect
+//         this.hasDeviceMotion = 'ondevicemotion' in window;
 
-        if (typeof options === 'object') {
-            for (var i in options) {
-                if (options.hasOwnProperty(i)) {
-                    this.options[i] = options[i];
-                }
-            }
-        }
+//         this.options = {
+//             threshold: 15, //default velocity threshold for shake to register
+//             timeout: 1000 //default interval between events
+//         };
 
-        //use date to prevent multiple shakes firing
-        this.lastTime = new Date();
+//         if (typeof options === 'object') {
+//             for (var i in options) {
+//                 if (options.hasOwnProperty(i)) {
+//                     this.options[i] = options[i];
+//                 }
+//             }
+//         }
 
-        //accelerometer values
-        this.lastX = null;
-        this.lastY = null;
-        this.lastZ = null;
+//         //use date to prevent multiple shakes firing
+//         this.lastTime = new Date();
 
-        //create custom event
-        if (typeof document.CustomEvent === 'function') {
-            this.event = new document.CustomEvent('shake', {
-                bubbles: true, 
-                cancelable: true
-            });
-        } else if (typeof document.createEvent === 'function') {
-            this.event = document.createEvent('Event');
-            this.event.initEvent('shake', true, true);
-        } else {
-            return false;
-        }
-    }
+//         //accelerometer values
+//         this.lastX = null;
+//         this.lastY = null;
+//         this.lastZ = null;
 
-    //reset timer values
-    Shake.prototype.reset = function () {
-        this.lastTime = new Date();
-        this.lastX = null;
-        this.lastY = null;
-        this.lastZ = null;
-    };
+//         //create custom event
+//         if (typeof document.CustomEvent === 'function') {
+//             this.event = new document.CustomEvent('shake', {
+//                 bubbles: true, 
+//                 cancelable: true
+//             });
+//         } else if (typeof document.createEvent === 'function') {
+//             this.event = document.createEvent('Event');
+//             this.event.initEvent('shake', true, true);
+//         } else {
+//             return false;
+//         }
+//     }
 
-    //start listening for devicemotion
-    Shake.prototype.start = function () {
-        this.reset();
-        if (this.hasDeviceMotion) {
-            window.addEventListener('devicemotion', this, false);
-        }
-    };
+//     //reset timer values
+//     Shake.prototype.reset = function () {
+//         this.lastTime = new Date();
+//         this.lastX = null;
+//         this.lastY = null;
+//         this.lastZ = null;
+//     };
 
-    //stop listening for devicemotion
-    Shake.prototype.stop = function () {
-        if (this.hasDeviceMotion) {
-            window.removeEventListener('devicemotion', this, false);
-        }
-        this.reset();
-    };
+//     //start listening for devicemotion
+//     Shake.prototype.start = function () {
+//         this.reset();
+//         if (this.hasDeviceMotion) {
+//             window.addEventListener('devicemotion', this, false);
+//         }
+//     };
 
-    //calculates if shake did occur
-    Shake.prototype.devicemotion = function (e) {
-        var current = e.accelerationIncludingGravity;
-        var currentTime;
-        var timeDifference;
-        var deltaX = 0;
-        var deltaY = 0;
-        var deltaZ = 0;
+//     //stop listening for devicemotion
+//     Shake.prototype.stop = function () {
+//         if (this.hasDeviceMotion) {
+//             window.removeEventListener('devicemotion', this, false);
+//         }
+//         this.reset();
+//     };
 
-        if ((this.lastX === null) && (this.lastY === null) && (this.lastZ === null)) {
-            this.lastX = current.x;
-            this.lastY = current.y;
-            this.lastZ = current.z;
-            return;
-        }
+//     //calculates if shake did occur
+//     Shake.prototype.devicemotion = function (e) {
+//         var current = e.accelerationIncludingGravity;
+//         var currentTime;
+//         var timeDifference;
+//         var deltaX = 0;
+//         var deltaY = 0;
+//         var deltaZ = 0;
 
-        deltaX = Math.abs(this.lastX - current.x);
-        deltaY = Math.abs(this.lastY - current.y);
-        deltaZ = Math.abs(this.lastZ - current.z);
+//         if ((this.lastX === null) && (this.lastY === null) && (this.lastZ === null)) {
+//             this.lastX = current.x;
+//             this.lastY = current.y;
+//             this.lastZ = current.z;
+//             return;
+//         }
 
-        if (((deltaX > this.options.threshold) && (deltaY > this.options.threshold)) || ((deltaX > this.options.threshold) && (deltaZ > this.options.threshold)) || ((deltaY > this.options.threshold) && (deltaZ > this.options.threshold))) {
-            //calculate time in milliseconds since last shake registered
-            currentTime = new Date();
-            timeDifference = currentTime.getTime() - this.lastTime.getTime();
+//         deltaX = Math.abs(this.lastX - current.x);
+//         deltaY = Math.abs(this.lastY - current.y);
+//         deltaZ = Math.abs(this.lastZ - current.z);
 
-            if (timeDifference > this.options.timeout) {
-                window.dispatchEvent(this.event);
-                this.lastTime = new Date();
-            }
-        }
+//         if (((deltaX > this.options.threshold) && (deltaY > this.options.threshold)) || ((deltaX > this.options.threshold) && (deltaZ > this.options.threshold)) || ((deltaY > this.options.threshold) && (deltaZ > this.options.threshold))) {
+//             //calculate time in milliseconds since last shake registered
+//             currentTime = new Date();
+//             timeDifference = currentTime.getTime() - this.lastTime.getTime();
 
-        this.lastX = current.x;
-        this.lastY = current.y;
-        this.lastZ = current.z;
+//             if (timeDifference > this.options.timeout) {
+//                 window.dispatchEvent(this.event);
+//                 this.lastTime = new Date();
+//             }
+//         }
 
-    };
+//         this.lastX = current.x;
+//         this.lastY = current.y;
+//         this.lastZ = current.z;
 
-    //event handler
-    Shake.prototype.handleEvent = function (e) {
-        if (typeof (this[e.type]) === 'function') {
-            return this[e.type](e);
-        }
-    };
+//     };
 
-    return Shake;
-}));
+//     //event handler
+//     Shake.prototype.handleEvent = function (e) {
+//         if (typeof (this[e.type]) === 'function') {
+//             return this[e.type](e);
+//         }
+//     };
+
+//     return Shake;
+// }));

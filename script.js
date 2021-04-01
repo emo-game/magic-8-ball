89d116f8d03d05b42ac27f1502f864351691f4b4
Léa <leareviron@yahoo.fr>
2021-04-01 11:23:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,4 @@
 
-// let CURRENT_SEQ = null
-
-// function Shake(options) {
-//     //feature detect
-//     this.hasDeviceMotion = 'ondevicemotion' in window;
-
-//     this.options = {
-//         threshold: 15, //default velocity threshold for shake to register
-//         timeout: 1000 //default interval between events
-//     };
-  
-
-// }
-// var myShakeEvent = new Shake({
-//     threshold: 15, // optional shake strength threshold
-//     timeout: 1000 // optional, determines the frequency of event generation
-// });
-
-// myShakeEvent.start();
-
-// window.addEventListener('shake', shakeEventDidOccur, false);
-
-
-
-
-
-// //function to call when shake occurs
-
-// function shakeEventDidOccur () {
-   
-  
-
-//     console.log("shake ya booty man!");
-
-// }
 
 var x, y, z;
 var px, py, pz; // p = precedent
